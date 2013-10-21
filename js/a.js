266eaf390fbfd24e23c44bddb2176351d0e7d1de
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2013-10-21 19:50:17 +0400

    Add func get_size_block.

--- js/a.js
+++ js/a.js
@@ -3,65 +3,79 @@ $(document).ready(function(){
 	// основные параметры
 	var count_col = 4;		// количество колонок
 	var distance = 10;		// расстояние между ячейками
+
+	var steck_length = [0,0,0,0];
 	
 
 	// вычисляемые параметры
 	var single_size = ($('.tile_content').width()-distance*(count_col-1))/4;
 
 
-	// начальные настройки
-	$('.b_itemtile_1_1').css({
-		'width':single_size,
-		'height':single_size
-	});
-
-	$('.b_itemtile_1_2').css({
-		'width':single_size,
-		'height':single_size*2+distance
-	});
-
-	$('.b_itemtile_2_1').css({
-		'width':single_size*2+distance,
-		'height':single_size
-	});
-
-	$('.b_itemtile_2_2').css({
-		'width':single_size*2+distance,
-		'height':single_size*2+distance
-	});
-
-	$('.b_itemtile_1_3').css({
-		'width':single_size,
-		'height':single_size*3+distance*2
-	});
-
-	$('.b_itemtile_3_1').css({
-		'width':single_size*3+distance*2,
-		'height':single_size
-	});
-
-	$('.b_itemtile_1_4').css({
-		'width':single_size,
-		'height':single_size*4+distance*3
-	});
-
-	$('.b_itemtile_4_1').css({
-		'width':single_size*4+distance*3,
-		'height':single_size
-	});
-
-
-
-	// ненужные настройки !!!! УДАЛИТЬ
-	$('.b_itemtile').css({
-		'margin-left':distance+'px',
-		'margin-bottom':distance+'px'
-	});
-	
+	start_size_init();
 	
 
+	// set position
+	// В steck_length запихиваем высоту каждого столбца.
+	// Надо определять минимальную высоту и смотеть где соседи не мешают
+	$('.b_itemtile').each(function(){
+
+	});
 
 
-	console.log(single_size);
+	// // ненужные настройки !!!! УДАЛИТЬ
+	// $('.b_itemtile').css({
+	// 	'margin-left':distance+'px',
+	// 	'margin-bottom':distance+'px'
+	// });
+	
+
+	// size initialization
+	function start_size_init()
+	{
+		$('.b_itemtile').each(function(){
+			var size = get_size_block( $(this) );
+			$(this).css({
+				'width':single_size*size[0]+distance*(size[0]-1),
+				'height':single_size*size[1]+distance*(size[1]-1)
+			});
+		});
+	};
+
+
+	// get array size block
+	function get_size_block( itm_block )
+	{
+		if( itm_block.hasClass('b_itemtile_1_1') ){
+			return [1,1];
+		};
+
+		if( itm_block.hasClass('b_itemtile_1_2') ){
+			return [1,2];
+		};
+
+		if( itm_block.hasClass('b_itemtile_2_1') ){
+			return [2,1];
+		};
+
+		if( itm_block.hasClass('b_itemtile_2_2') ){
+			return [2,2];
+		};
+
+		if( itm_block.hasClass('b_itemtile_1_3') ){
+			return [1,3];
+		};
+
+		if( itm_block.hasClass('b_itemtile_3_1') ){
+			return [3,1];
+		};
+
+		if( itm_block.hasClass('b_itemtile_1_4') ){
+			return [1,4];
+		};
+
+		if( itm_block.hasClass('b_itemtile_4_1') ){
+			return [4,1];
+		};
+	};
 
 });
